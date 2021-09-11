8c502fa41104267006230e6f2dcd2f5ffa99df8b
Antti Nilsson <muut.py@antion.fi>
2021-09-11 20:36:50 +0300

    v1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anoptions",
-    version="1.0.3",
+    version="1.0.4",
     author="anttin",
     author_email="muut.py@antion.fi",
     description="Module to assist in defining application options and collecting user input for them from command line and environment variables.",
